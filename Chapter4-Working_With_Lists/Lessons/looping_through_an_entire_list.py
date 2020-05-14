d325847421f1d9d4de3988a04d6d75ca00a7df2d
tomagrab <tomaarongrabowski@gmail.com>
2020-05-13 22:14:47 -0400

    Edited Chapter 4 lesson 1

--- Chapter4-Working_With_Lists/Lessons/looping_through_an_entire_list.py
+++ Chapter4-Working_With_Lists/Lessons/looping_through_an_entire_list.py
@@ -2,3 +2,7 @@
 magicians = ["alice", "david", "carolina"]
 for magician in magicians:
     print(magician)
+
+# Do more work
+for magician in magicians:
+    print(magician.title() + ", that was an amazing trick!")
